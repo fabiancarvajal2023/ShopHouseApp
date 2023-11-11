6718dbb7f49847804aec518a14bccfab02a59463
fabiancarvajal2023 <wilson.carvajal@arandasoft.com>
2023-11-10 23:56:58 -0500

    creando pantalla principal

---
+++ app/src/main/java/com/fabian/mobile/shophouseapp/main/ui/screens/search/SearchBar.kt
@@ -0,0 +1,123 @@
+package com.fabian.mobile.shophouseapp.main.ui.screens.search
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fabian.mobile.shophouseapp.main.ui.screens.textfield.CustomBasicTextField
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier,
+    searchText: String,
+    isEmpty: Boolean,
+    onValueChange: (String) -> Unit,
+    onEmptyOnClick: () -> Unit,
+    onCloseOnClick: () -> Unit
+) {
+    CustomBasicTextField(
+        modifier = modifier.clip(RoundedCornerShape(percent = 50)),
+        value = searchText,
+        onValueChange = onValueChange,
+        textStyle = TextStyle(fontSize = 12.sp),
+        singleLine = true,
+        maxLines = 1,
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = Icons.Default.Search,
+                contentDescription = ""
+            )
+        },
+        trailingIcon = {
+            if (isEmpty) {
+                IconButton(
+                    onClick = onCloseOnClick, colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Gray,
+                        disabledContainerColor = Color.Transparent,
+                        disabledContentColor = Color.Gray
+                    )
+                ) {
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        contentDescription = ""
+                    )
+                }
+            } else {
+                IconButton(
+                    onClick = onEmptyOnClick, colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Gray,
+                        disabledContainerColor = Color.Transparent,
+                        disabledContentColor = Color.Gray
+                    )
+                ) {
+                    Icon(
+                        modifier = Modifier.size(20.dp),
+                        imageVector = Icons.Default.Close,
+                        contentDescription = ""
+                    )
+                }
+            }
+        },
+        placeholder = {
+            Text(
+                text = "Buscar en shophouse",
+                color = Color.Gray,
+                fontSize = 12.sp
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            cursorColor = Color.Gray,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        contentPadding = PaddingValues(
+            start = 15.dp,
+            end = 10.dp,
+            top = 0.dp,
+            bottom = 0.dp
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun preview()
+{
+    Column(modifier = Modifier.fillMaxSize().background(Color.Red)) {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            searchText = "",
+            isEmpty = true,
+            onValueChange = {},
+            onEmptyOnClick = { /*TODO*/ }) {
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/fabian/mobile/shophouseapp/main/ui/screens/textfield/CustomBasicTextField.kt
@@ -0,0 +1,89 @@
+package com.fabian.mobile.shophouseapp.main.ui.screens.textfield
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TextFieldDefaults.textFieldWithoutLabelPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomBasicTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = TextFieldDefaults.filledShape,
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors(),
+    contentPadding: PaddingValues =
+        if (label == null) {
+            textFieldWithoutLabelPadding()
+        } else {
+            TextFieldDefaults.textFieldWithLabelPadding()
+        }
+
+) {
+    // If color is not provided via the text style, use content color as a default
+    val textColor = textStyle.color
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+    BasicTextField(
+        value = value,
+        modifier = modifier,
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = mergedTextStyle,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        interactionSource = interactionSource,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        decorationBox = @Composable { innerTextField ->
+            // places leading icon, text field with label and placeholder, trailing icon
+            TextFieldDefaults.TextFieldDecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                label = label,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                supportingText = supportingText,
+                shape = shape,
+                singleLine = singleLine,
+                enabled = enabled,
+                isError = isError,
+                interactionSource = interactionSource,
+                colors = colors,
+                contentPadding = contentPadding
+            )
+        }
+    )
+
+}
